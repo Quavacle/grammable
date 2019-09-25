9818059ab7c7032b7244d68d817f4aeaafd11657
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-09-25 04:47:26 +0000

    removed indexes for heroku test

--- db/migrate/20190920201312_create_comments.rb
+++ db/migrate/20190920201312_create_comments.rb
@@ -6,8 +6,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     t.integer :user_id
     t.integer :gram_id
     t.datetime
-    add_index :comments, :user_id
-    add_index :comments, :gram_id 
+    
   end  
   end
 end
